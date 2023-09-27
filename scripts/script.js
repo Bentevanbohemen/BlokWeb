ef6b9dc0f081dbd17432db759d6568c6eafcc2ad
Bentevanbohemen <144136603+Bentevanbohemen@users.noreply.github.com>
2023-09-27 13:51:51 +0200

    fjnfjfn

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 console.log("hi");
+
+var HamburgerButton = document.querySelector("button")
+var Hamburgerul = document.querySelector("ul")
+
+HamburgerButton.onclick = function () {
+    Hamburgerul.classList.toggle ("open")
+
+}
